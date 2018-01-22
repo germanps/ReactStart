6f0b39906c01af47c5148e6b55d478abe3df68fd
Germán Pla <germanps@outlook.es>
2018-01-22 17:15:37 +0100

    Import react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+//Import React
+import React from 'react';
+//import ReactDom
+import ReactDOM from 'react-dom';
+
 // Crate a nuew component. This component should produce some HTML
 const App = function() {
 	return <div> Hi! </div>;
 }
 
-
 //Take this component's generated HTML and put it on the page in the DOM
-
-//Prueba git develop
+React.render(<App >);
