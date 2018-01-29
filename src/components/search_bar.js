62909048d48c575d746a8e37d8e611f6504d08fe
Germán Pla <germanps@outlook.es>
2018-01-29 15:27:00 +0100

    component state (this.state)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,11 +5,21 @@ import React, { Component } from 'react';
 	return <input />
 }*/
 class SearchBar extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = { term: '' };
+	}
 	/*render() {
 		return <input onChange={this.onInputChange}/>
 	}*/
 	render() {
-		return <input onChange={event => console.log(event.target.value) } />;
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value}) } /><br />
+				<span>Value of the input: {this.state.term} </span> 
+			</div>
+		);
 	}
 
 	/*onInputChange(event) {
