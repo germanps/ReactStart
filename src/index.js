6de0f2bf784579968fd3648e3a505ae5fc89c748
Germán Pla <germanps@outlook.es>
2018-01-29 10:48:28 +0100

    first render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-//Import React
 import React from 'react';
-//import ReactDom
 import ReactDOM from 'react-dom';
 
 // Crate a nuew component. This component should produce some HTML
-const App = function() {
+/*const App = function() {
 	return <div> Hi! </div>;
+}*/
+const App = () => {
+	return <div> Hi! devs </div>;
 }
 
 //Take this component's generated HTML and put it on the page in the DOM
-React.render(<App >);
+ReactDOM.render(<App />, document.querySelector('.container'));
