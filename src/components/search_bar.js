5056c59b753a59f5ae1463d84773b134f500cf55
Germán Pla <germanps@outlook.es>
2018-01-29 14:18:27 +0100

    search component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,19 @@
-search_bar.js
+import React, { Component } from 'react';
+
+
+/*const SearchBar = () => {
+	return <input />
+}*/
+class SearchBar extends Component{
+	render() {
+		return <input onChange={this.onInputChange}/>
+	}
+
+	onInputChange() {
+		//whenever the input changes, run the code inside here
+
+	}
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+//Api youtube key
+const API_KEY = 'AIzaSyB5qZ1oTgSYt1dM6df0Om8Yxgrpj_hoABo';
 
 // Crate a nuew component. This component should produce some HTML
 /*const App = function() {
 	return <div> Hi! </div>;
 }*/
 const App = () => {
-	return <div> Hi! devs </div>;
+	return (
+			<div> 
+				<SearchBar />
+		   </div>
+    );
 }
 
 //Take this component's generated HTML and put it on the page in the DOM
